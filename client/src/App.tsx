c6eca2ea7254c9ab6fa46b6bc5da5221f42e3993
RussianCabbageSoup <testylya333@gmail.com>
2026-09-17 06:47:44 +0300

    add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+
 const App = () => {
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
